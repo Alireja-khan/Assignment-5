970c8fbbcdf21f1b81a1c63ee2396f5a70c6b836
Alireja-khan <alirejakhan18@gmail.com>
2025-03-01 21:24:48 +0600

    added extra html page with linking them and added an alert after click complete btn.

---
+++ script/scrip.js
@@ -0,0 +1,10 @@
+
+document.getElementById("Discover").addEventListener("click", function(){
+    window.location.href = "./blog.html";
+})
+
+
+document.getElementById("Complete-btn-1").addEventListener("click", function(){
+    alert("Are You Sure?");
+})
+
